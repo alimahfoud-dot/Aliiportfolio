d139f243f49ad737ef012f01bbc4fb7ccf3fb778
alimahfoud-dot <alimahfoud883@gmail.com>
2026-03-15 18:13:23 +0300

    Add files via upload

---
+++ ali.js
@@ -0,0 +1,43 @@
+const quoteElement = document.querySelector('.typing-quote');
+if (quoteElement) {
+  const fullText = quoteElement.textContent.trim();  // "Hi, I'm Ali" (بدون العلامات إذا بدك)
+  let index = fullText.length;
+  let isDeleting = true;
+
+  function animate() {
+    if (isDeleting) {
+      quoteElement.textContent = fullText.substring(0, index);
+      index--;
+      if (index < 0) {
+        index = 0;
+        isDeleting = false;
+      }
+    } else {
+      quoteElement.textContent = fullText.substring(0, index);
+      index++;
+      if (index > fullText.length) {
+        index = fullText.length;
+        isDeleting = true;
+      }
+    }
+
+    const speed = isDeleting ? 40 : 80;
+    setTimeout(animate, speed);
+  }
+
+  // 
+  setTimeout(animate, 800);
+}
+const text = document.querySelector('.marquee-text');
+let pos = -text.offsetWidth; // ا
+
+function moveMarquee() {
+    pos += 0.3; //
+    if (pos >100) {
+        pos = -text.offsetWidth; // 
+    }
+    text.style.left = pos + '%';
+    requestAnimationFrame(moveMarquee);
+}
+
+moveMarquee();
